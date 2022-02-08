058bfcb3d3aae19959f4ec4abf0d20471ee4eb43
MjRK <sidorenko6503@gmail.com>
2022-02-08 12:18:53 +0300

    fisrt commit

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,28 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img className = 'content-background' src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg" alt=""/>
+            </div>
+            <div>
+                ava+description
+            </div>
+            <div>
+                My Posts
+                <div>
+                    New Posts
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
