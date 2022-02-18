4c8e33f7a6149b52b9ead71643e1e14fcfb11fbc
MjRK <sidorenko6503@gmail.com>
2022-02-18 13:22:02 +0300

    Introduction to Props in React

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -10,11 +10,8 @@ const MyPosts = () => {
                     <button>Remove</button>
                 </div>
                 <div className = 'posts'>
-                   <Post/>
-                   <Post/>
-                   <Post/>
-                   <Post/>
-                   <Post/>
+                   <Post message = 'Hi, how are you?'/>
+                   <Post message = "It's my first post"/>
 
                 </div>
             </div>
