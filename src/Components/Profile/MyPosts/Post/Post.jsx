acf5a433b4bcd6d63bc2c156988d9c29643434f2
MjRK <sidorenko6503@gmail.com>
2022-02-18 13:22:02 +0300

    Lesson 16. Introduction to Props in React

--- src/Components/Profile/MyPosts/Post/Post.jsx
+++ src/Components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,12 @@
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
     return (
             <div>
 
                 <div className = {s.item}>
                     <img src="https://avatars.mds.yandex.net/get-kino-vod-films-gallery/28788/47e2fd514411e18b76af786d7417062d/100x64_3" alt=""/>
-                    Post1
+                    {props.message}
                     <div>
                         <span>like</span>
                     </div>
