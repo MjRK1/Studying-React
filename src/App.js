733d5b8299829424750f2ccd60973d3986d5ad0f
MjRK <sidorenko6503@gmail.com>
2022-03-21 10:50:14 +0300

    lesson 19. Studying React routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Header from "./Components/Header/Header";
 import Nav from "./Components/Navbar/Nav";
 import Dialogs from "./Components/Dialogs/Dialogs";
 import Profile from "./Components/Profile/Profile";
+import {Routes, Route, Link} from "react-router-dom";
 
-
-const App = () => {
+const App = (props) => {
     return (
+
         <div className='app-wrapper'>
             <Header/>
             <Nav/>
             <div class='app-wrapper-content'>
-                <Dialogs/>
+                <Routes>
+                    <Route path={"/dialogs"} element={<Dialogs/>}/>
+                    <Route path={"/profile"} element={<Profile/>}/>
+                </Routes>
+
             </div>
-            {/*<Profile/>*/}
         </div>
     );
 }
--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -1,19 +1,23 @@
 import s from './Nav.module.css'
+import {Link} from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className= {s.nav}>
-            <div className = {s.item}>
-                <a>Profile</a>
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <Link to="/profile" className={s.link}>Profile</Link>
             </div>
-            <div className = {`${s.item} ${s.active}`}>
-                <a>Messages</a>
+            <div className={`${s.item} ${s.active}`}>
+                <Link to="/dialogs" className={s.link}>Messages</Link>
             </div>
-            <div className = {s.item}>
-                <a>News</a>
+            <div className={s.item}>
+                <Link to="" className={s.link}>News</Link>
             </div>
-            <div className = {s.item}>
-                <a>Music</a>
+            <div className={s.item}>
+                <Link to="" className={s.link}>Music</Link>
+            </div>
+            <div className={s.item}>
+                <Link to="" className={s.link}>Settings</Link>
             </div>
         </nav>
     );
