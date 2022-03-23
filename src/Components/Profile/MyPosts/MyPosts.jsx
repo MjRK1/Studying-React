57abcfc2eb19eb4b4abf0b7f3c18dabc02333534
MjRK <sidorenko6503@gmail.com>
2022-03-24 00:55:09 +0300

    Lesson 24. UI and BLL, push data aside.

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let postData = [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 1, message: 'It\' my first post', likesCount: 11}
+    ]
     return (
         <div className={s.postsBlocks}>
             My Posts
@@ -11,10 +15,9 @@ const MyPosts = () => {
                 <button className={s.removePostButton}>Remove</button>
             </div>
             <div className={s.posts}>
-                <Post className = {postMessage} message='Hi, how are you?' likesCount="0"/>
-                <Post className = {postMessage} message="It's my first post" likesCount="23"/>
-                <Post className = {postMessage} message='Hi, how are you?'/>
-                <Post className = {postMessage} message="It's my first post"/>
+                <Post className = {postMessage} message={postData[0].message} likesCount={postData[0].likesCount}/>
+                <Post className = {postMessage} message={postData[1].message} likesCount={postData[1].likesCount}/>
+
             </div>
         </div>
     );
