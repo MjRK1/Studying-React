3e74446fe3f82f851600f7424fb77825fa5c98a6
MjRK <sidorenko6503@gmail.com>
2022-04-13 22:08:09 +0300

    Lesson 35. Got rid of render.js. Have refactored code.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './index.css';
-import state from "./redux/state";
-import {rerenderEntireTree} from "./render";
+import state, {addPost, updateNewPostText} from "./redux/state";
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import {subscribe} from "./redux/state";
 
-
-
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App appState={state}
+                     addPost={addPost}
+                     updateNewPostText={updateNewPostText}/>
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 rerenderEntireTree(state);
-
+subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
