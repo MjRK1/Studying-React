60a94a06f7d4a7b4c905075161b0046cf30db426
MjRK <sidorenko6503@gmail.com>
2022-05-20 15:51:58 +0300

    Lesson 42. Introduction to Redux.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import store from "./redux/state";
+import store from "./redux/redux-store";
 import reportWebVitals from './reportWebVitals';
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
@@ -19,7 +19,11 @@ let rerenderEntireTree = (state) => {
 }
 
 rerenderEntireTree(store.getState());
-store.subscribe(rerenderEntireTree);
+store.subscribe(() => {
+    let state = store.getState();
+    rerenderEntireTree(state);
+
+});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/sidebar-reducer.js
+++ src/redux/sidebar-reducer.js
@@ -1,4 +1,13 @@
-export const sidebarReducer = (state, action) => {
+let initialState = {
+    friendsList: [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrew'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'}
+    ]
+}
+
+export const sidebarReducer = (state=initialState, action) => {
     switch (action.type) {
         default:
             return state;
