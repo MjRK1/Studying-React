82c4a0e267fe44f8d16e4beebd26d6024e1f622a
MjRK <sidorenko6503@gmail.com>
2022-07-20 13:19:42 +0300

    Lesson 45. Connect, mapStateToProps, mapDispatchToProps.

--- src/Components/Navbar/Nav.jsx
+++ src/Components/Navbar/Nav.jsx
@@ -1,36 +1,29 @@
 import s from './Nav.module.css'
 import {NavLink} from "react-router-dom";
 import Friends from "./FriendList/Friends";
-import StoreContext from "../../StoreContext";
+import store from "../../redux/redux-store";
 
 const setActive = ({isActive}) => isActive ? s.active : s.item;
 
 const Nav = (props) => {
-    return <StoreContext.Consumer>
-        {
-            (store) => {
-                return (
-                    <nav className={s.nav}>
-                        <div className={s.item}>
-                            <NavLink to="/profile" className={setActive}>Profile</NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/dialogs" className={setActive}>Messages</NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/news" className={setActive}>News</NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/music" className={setActive}>Music</NavLink>
-                        </div>
-                        <div className={s.item}>
-                            <NavLink to="/settings" className={setActive}>Settings</NavLink>
-                        </div>
-                        <Friends friendsList={store.getState().sidebarData.friendsList}/>
-                    </nav>
-                );
-            }
-        }
-    </StoreContext.Consumer>
+    return (
+        <nav className={s.nav}>
+            <div className={s.item}>
+                <NavLink to="/profile" className={setActive}>Profile</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/dialogs" className={setActive}>Messages</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/news" className={setActive}>News</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/music" className={setActive}>Music</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/settings" className={setActive}>Settings</NavLink>
+            </div>
+            <Friends friendsList={store.getState().sidebarData.friendsList}/>
+        </nav>);
 }
 export default Nav;
--- src/Components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/Components/Profile/MyPosts/MyPostsContainer.jsx
@@ -2,34 +2,29 @@ import React from "react";
 
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
-import StoreContext from "../../../StoreContext";
+import {connect} from "react-redux";
 
-
-const MyPostsContainer = () => {
-    return (
-        <StoreContext.Consumer>
-            {
-                (store) => {
-                    let state = store.getState();
-                    let addPost = () => {
-                        store.dispatch(addPostActionCreator());
-                    }
-                    let removePostText = () => {
-                        store.dispatch(updateNewPostTextActionCreator(''));
-                    }
-
-                    let onPostChange = (text) => {
-                        let action = updateNewPostTextActionCreator(text);
-                        store.dispatch(action);
-                    }
-                    return <MyPosts updateNewPostText={onPostChange}
-                                    removePost={removePostText}
-                                    addPost={addPost}
-                                    posts={state.profilePage.posts}
-                                    newPostText={state.profilePage.newPostText}/>
-                }
-            }
-        </StoreContext.Consumer>
-    );
+let mapStateToProps = (state) => {
+    return {
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+    }
 }
+let mapDispatchToProps = (dispatch) => {
+    return {
+        updateNewPostText: (text) => {
+            let action = updateNewPostTextActionCreator(text);
+            dispatch(action);
+        },
+        removePost: () => {
+            dispatch(updateNewPostTextActionCreator(''));
+        },
+        addPost: () => {
+            dispatch(addPostActionCreator())
+        }
+    }
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 export default MyPostsContainer;
